068850c17b256116a5c9041c94864d80303488ce
mdoblas <61249386+mdoblas@users.noreply.github.com>
2026-05-10 00:48:19 +0200

    Update custom-carousel.js
    Se añade tratamiento para que se muestren las imágenes al hacer click sobre una, solo del contenedor de la clase gallery-image-container,

--- custom-carousel.js
+++ custom-carousel.js
@@ -1,113 +1,130 @@
-            jQuery(document).ready(function () {
-                var mobile_device = isMobileDevice();
-                var galleryImages = jQuery('.gallery-image');
+jQuery(document).ready(function () {
+	var mobile_device = isMobileDevice();
+	var allGalleryImages = jQuery('.gallery-image');
 
-                if (galleryImages.length > 0 && !mobile_device) {
+	if (allGalleryImages.length > 0 && !mobile_device) {
 
-                    jQuery("body").append("<div class='lightbox' style='display: none;'><div class='nav'><button id='prevButton'>&lt;</button><button id='nextButton'>&gt;</button></div><img id='lightboxImage' src='' alt='Imagen ampliada'><p id='lightbox_img_counter'></p><span class='close-button' id='closeButton'>×</span></div>");
+		jQuery("body").append("<div class='lightbox' style='display: none;'><div class='nav'><button id='prevButton'>&lt;</button><button id='nextButton'>&gt;</button></div><img id='lightboxImage' src='' alt='Imagen ampliada'><p id='lightbox_img_counter'></p><span class='close-button' id='closeButton'>×</span></div>");
 
-                    if (galleryImages.length === 1) {
-                        jQuery(".lightbox .nav button").css("display", "none");
-                    }
+		var galleryImages = allGalleryImages;
+		var currentIndex = 0;
 
-                    jQuery(".lightbox .nav button, .lightbox .close-button").hover(function () {
-                        jQuery(this).css("color", "white");
-                    }, function () {
-                        jQuery(this).css("color", "#999");
-                    });
+		allGalleryImages.click(function () {
+			galleryImages = getImagesForCarousel(this);
+			currentIndex = galleryImages.index(this);
 
-                    var currentIndex = 0;
+			updateNavVisibility();
+			showLightbox(currentIndex);
 
-                    // Mostrar la imagen ampliada al hacer clic
-                    galleryImages.click(function () {
-                        currentIndex = galleryImages.index(this);
-                        showLightbox(currentIndex);
-                        jQuery('body').addClass('lock-scroll');
-                    });
+			jQuery('body').addClass('lock-scroll');
+		});
 
-                    // Mostrar la imagen ampliada en la luz de fondo
-                    function showLightbox(index) {
-                        var lightbox = jQuery('.lightbox');
-                        var lightboxImage = jQuery('#lightboxImage');
+		function getImagesForCarousel(clickedImage) {
+			var container = jQuery(clickedImage).closest('.gallery-image-container');
 
-                        var currentImage = galleryImages.eq(index);
-                        var imageUrl = currentImage.attr('src');
-                        lightboxImage.attr('src', imageUrl);
+			if (container.length) {
+				return container.find('.gallery-image');
+			}
 
-                        jQuery("#lightbox_img_counter").text((index + 1) + "/" + galleryImages.length);
+			return allGalleryImages.filter(function () {
+				return jQuery(this).closest('.gallery-image-container').length === 0;
+			});
+		}
 
-                        lightbox.fadeIn();
-                    }
+		function updateNavVisibility() {
+			if (galleryImages.length <= 1) {
+				jQuery(".lightbox .nav button").css("display", "none");
+			} else {
+				jQuery(".lightbox .nav button").css("display", "");
+			}
+		}
 
-                    // Cambiar a la imagen anterior
-                    jQuery('#prevButton').click(prevImgCustomCarousel);
+		jQuery(".lightbox .nav button, .lightbox .close-button").hover(function () {
+			jQuery(this).css("color", "white");
+		}, function () {
+			jQuery(this).css("color", "#999");
+		});
 
-                    // Cambiar a la siguiente imagen
-                    jQuery('#nextButton').click(nextImgCustomCarousel);
+		function showLightbox(index) {
+			var lightbox = jQuery('.lightbox');
+			var lightboxImage = jQuery('#lightboxImage');
 
-                    // Cerrar la vista de luz de fondo
-                    jQuery('#closeButton').click(function () {
-                        jQuery('body').removeClass('lock-scroll');
-                        jQuery('.lightbox').fadeOut();
-                    });
+			var currentImage = galleryImages.eq(index);
+			var imageUrl = currentImage.attr('src');
 
-                    //  Event listener para la tecla "Escape"
-                    document.addEventListener("keydown", function (event) {
-                        switch (event.key) {
-                            case "Escape":
-                                jQuery('body').removeClass('lock-scroll');
-                                jQuery('.lightbox').css("display", "none");
-                                break;
-                            case "ArrowRight":
-                                nextImgCustomCarousel();
-                                break;
-                            case "ArrowLeft":
-                                prevImgCustomCarousel();
-                                break;
-                            default:
-                                break;
-                        }
-                    });
+			lightboxImage.attr('src', imageUrl);
+			jQuery("#lightbox_img_counter").text((index + 1) + "/" + galleryImages.length);
 
-                    function prevImgCustomCarousel() {
-                        currentIndex--;
-                        if (currentIndex < 0) {
-                            currentIndex = galleryImages.length - 1;
-                        }
-                        showLightbox(currentIndex);
-                    }
+			lightbox.fadeIn();
+		}
 
-                    function nextImgCustomCarousel() {
-                        currentIndex++;
-                        if (currentIndex >= galleryImages.length) {
-                            currentIndex = 0;
-                        }
-                        showLightbox(currentIndex);
-                    }
+		jQuery('#prevButton').click(prevImgCustomCarousel);
+		jQuery('#nextButton').click(nextImgCustomCarousel);
 
-                }
+		jQuery('#closeButton').click(function () {
+			jQuery('body').removeClass('lock-scroll');
+			jQuery('.lightbox').fadeOut();
+		});
 
-                function isMobileDevice() {
-                    if (screen.width <= 767) {
-                        return true;
-                    }
+		document.addEventListener("keydown", function (event) {
+			if (!jQuery('.lightbox').is(':visible')) return;
 
-                    const agent = navigator.userAgent.toLowerCase();
-                    const keywordsMovil = ['mobile', 'android', 'iphone', 'ipod', 'blackberry', 'windows phone'];
-                    const keywordsTablet = ['tablet', 'ipad', 'android'];
+			switch (event.key) {
+				case "Escape":
+					jQuery('body').removeClass('lock-scroll');
+					jQuery('.lightbox').css("display", "none");
+					break;
+				case "ArrowRight":
+					nextImgCustomCarousel();
+					break;
+				case "ArrowLeft":
+					prevImgCustomCarousel();
+					break;
+			}
+		});
 
-                    for (let keyword of keywordsMovil) {
-                        if (agent.indexOf(keyword) !== -1) {
-                            return true;
-                        }
-                    }
+		function prevImgCustomCarousel() {
+			currentIndex--;
 
-                    for (let keyword of keywordsTablet) {
-                        if (agent.indexOf(keyword) !== -1) {
-                            return true;
-                        }
-                    }
+			if (currentIndex < 0) {
+				currentIndex = galleryImages.length - 1;
+			}
 
-                    return false;
-                }
-            });
+			showLightbox(currentIndex);
+		}
+
+		function nextImgCustomCarousel() {
+			currentIndex++;
+
+			if (currentIndex >= galleryImages.length) {
+				currentIndex = 0;
+			}
+
+			showLightbox(currentIndex);
+		}
+	}
+
+	function isMobileDevice() {
+		if (screen.width <= 767) {
+			return true;
+		}
+
+		const agent = navigator.userAgent.toLowerCase();
+		const keywordsMovil = ['mobile', 'android', 'iphone', 'ipod', 'blackberry', 'windows phone'];
+		const keywordsTablet = ['tablet', 'ipad', 'android'];
+
+		for (let keyword of keywordsMovil) {
+			if (agent.indexOf(keyword) !== -1) {
+				return true;
+			}
+		}
+
+		for (let keyword of keywordsTablet) {
+			if (agent.indexOf(keyword) !== -1) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+});
